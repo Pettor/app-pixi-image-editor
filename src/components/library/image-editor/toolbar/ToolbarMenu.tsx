163ffdd6791e6fe2d4a4c877d943c114c0295856
Petter Hancock <pettor@gmail.com>
2024-02-16 07:57:42 +0100

    fix: tailwind lint issues

--- src/components/library/image-editor/toolbar/ToolbarMenu.tsx
+++ src/components/library/image-editor/toolbar/ToolbarMenu.tsx
@@ -35,7 +35,7 @@ export function ToolbarMenu({ onNewImage }: ToolbarMenuProps): ReactElement {
 
   return (
     <div className="absolute z-20 flex items-center justify-center p-4 max-sm:bottom-0 max-sm:right-0">
-      <div className="glass navbar flex h-10 min-h-0 rounded-box bg-base-300 bg-opacity-80 shadow-xl dark:bg-opacity-80 dark:bg-none md:h-12">
+      <div className="glass navbar flex h-10 min-h-0 rounded-box bg-base-300 bg-opacity-80 shadow-xl md:h-12 dark:bg-opacity-80 dark:bg-none">
         <button className="btn btn-square btn-ghost btn-sm" onClick={handleMenuClick}>
           <Bars3Icon className="h-4 w-4 md:h-6 md:w-6" />
         </button>
