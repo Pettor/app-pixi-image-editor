3f24025268a453956d902755cd876f02aec4ac12
Petter Hancock <pettor@gmail.com>
2024-02-20 09:49:03 +0100

    chore: remove tailwind prettier sorting plugin

--- src/components/library/image-editor/toolbar/ToolbarZoom.tsx
+++ src/components/library/image-editor/toolbar/ToolbarZoom.tsx
@@ -7,7 +7,7 @@ export function ToolbarZoom(): ReactElement {
 
   return (
     <div className="absolute bottom-0 left-0 z-10 flex justify-center p-4">
-      <div className="glass navbar h-10 min-h-0 w-auto gap-4 rounded-box border border-neutral-content bg-base-100 bg-opacity-60 shadow-xl dark:bg-neutral dark:bg-opacity-80 dark:bg-none md:h-12">
+      <div className="glass navbar h-10 min-h-0 w-auto gap-4 rounded-box border border-neutral-content bg-base-100 bg-opacity-60 shadow-xl md:h-12 dark:bg-neutral dark:bg-opacity-80 dark:bg-none">
         <button className="btn btn-square btn-ghost btn-sm" onClick={zoomIn}>
           <PlusIcon />
         </button>
